1a9ca7d3104dc048cafa6ffa5d15db8fc6fa84fc
keith <keith.wachira@students.moringaschol.com>
2023-10-27 13:10:40 +0300

    link readmemd

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
             breeds.push("random");
             breedsSelect.innerHTML = breeds.map((breed) => `<option value="${breed}">${breed}</option>`).join("");
         })
-        .catch((error) => console.error("Error fetching breeds:", error));
+        .catch((error) => console.error("Error fetching breeds: ", error));
 
     // Function to fetch and display a random dog image
     function fetchRandomDog() {
